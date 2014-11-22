bdc03c8e47162dc7427d33f73e8838e99dc9ec54
Fabrice Doré <Fabrice@unknown-4c:8d:79:f2:d9:f6.home>
2014-11-22 15:11:35 +0000

    Deleted build folder

--- build/js/homepage/homepageController.js
+++
@@ -1,9 +0,0 @@
-define(["require", "exports"], function (require, exports) {
-    var HomePageController = (function () {
-        function HomePageController($scope) {
-            $scope.message = 'Welcome';
-        }
-        return HomePageController;
-    })();
-    exports.HomePageController = HomePageController;
-});
